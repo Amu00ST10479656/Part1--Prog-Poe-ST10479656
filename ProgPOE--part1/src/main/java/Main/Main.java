a2ade6ce35c823933d3c20a00d733a47fc8c0b4d
Amukelisiwe Mtolo <ST10479656@myemeris.edu.za>
2026-04-14 01:36:15 +0200

    Fixing classes

--- ProgPOE--part1/src/main/java/Main/Main.java
+++ ProgPOE--part1/src/main/java/Main/Main.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Login;
+package Main;
 
 /**
  *
  * @author amuke
  */
-public class Test {
+public class Main {
     
 }
