e30739ee89d0c6e0b03b072f61b2626621d38b10
Amukelisiwe Mtolo <ST10479656@myemeris.edu.za>
2026-04-14 13:34:54 +0200

    Main Class

--- ProgPOE--part1/src/main/java/Main/Main.java
+++ ProgPOE--part1/src/main/java/Main/Main.java
@@ -4,10 +4,51 @@
  */
 package Main;
 
-/**
- *
- * @author amuke
- */
+import java.util.Scanner;
+import Login.Login;
+
+
 public class Main {
     
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to QuickChat \n");
+        
+        // Collect registration info
+        System.out.print("Enter First Name: ");
+        String firstName = scanner.nextLine();
+        
+        System.out.print("Enter Last Name: ");
+        String lastName = scanner.nextLine();
+        
+        System.out.print("Enter Username (must contain _ and be max 5 chars):");
+        String username = scanner.nextLine();
+        
+        System.out.print("Enter Password (min 8 chars, 1 capital, 1 number, 1 special char): ");
+        String password = scanner.nextLine();
+        
+        System.out.print("Enter Cell Phone Number (with international code e.g +278376251938): ");
+        String cellPhone = scanner.nextLine();
+        
+        //Register user
+        Login user = new Login(firstName, lastName, username, password, cellPhone);
+        System.out.println("\n--- Registration Status ---");
+        System.out.println(user.registerUser());
+        
+        //Login
+        System.out.print("\n--- Login ---");
+        System.out.print("Enter Username: ");
+        String loginUsername = scanner.nextLine();
+        
+        System.out.print("Enter Password: ");
+        String loginPassword = scanner.nextLine();
+        
+        System.out.println("\n--- Login Status ---");
+        System.out.println(user.returnLoginStatus(loginUsername, loginPassword));
+        
+        scanner.close();
+    }
+             
+                     
+                
 }
